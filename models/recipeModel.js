114da7745884622437bf125aa89e4f14770b1959
Siya021 <siyamthandamazokwana@gmail.com>
2024-04-30 16:08:46 +0200

    save

--- models/recipeModel.js
+++ models/recipeModel.js
@@ -1,11 +1,39 @@
 const mongoose = require ('mongoose');
-const dbConfig = require ('../config/db.config');
 
-mongoose.Promise = global.Promise;
+const recipeSchema = new mongoose.Schema({
+    title: {
+        type: String, required: true
+    },
+    ingredients:{
+        type: String, required: true
+    },
+    instructions:{
+        type: String, required: true
+    },
+    mealType:{
+        type: String, required: true
+    },
+    cuisine:{
+        type: String, required: true
+    },
+    difficulty:{
+        type: String, required: true
+    },
+    PrepTime:{
+        type: String, required: true
+    },
+    cookTime:{
+        type: String, required: true
+    },
+    servings:{
+        type: String, required: true
+    },
+    image:{
+        type: String, required: true
+    },
+    videoLink:{
+        type: String, required: true
+    },
+});
 
-const db = {};
-
-db.mongoose = mongoose;
-db.user = require("./user.model")(mongoose);
-
-module.exports = db;
+module.exports = mongoose.model('recipeModel', recipeSchema)
