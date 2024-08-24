ce96d048d3136224e07602b0401f43a10cac24df
buhleqampi <buhle3857@gmail.com>
2024-08-24 13:54:24 +0200

    CHANGE IN PORT

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const recipeRoutes = require("./routes/recipeRoutes");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 require('dotenv').config();
 // const connectionString = require("./config/db.config")
 
